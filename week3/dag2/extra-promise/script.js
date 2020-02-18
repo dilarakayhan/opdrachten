766a19be3ee508155e3104058b86ed3fe2254808
Dilara Kayhan <53559729+dilarakayhan@users.noreply.github.com>
2020-02-18 15:35:08 +0100

    aded w3d2-4

--- week3/dag2/extra-promise/script.js
+++ week3/dag2/extra-promise/script.js
@@ -1,6 +1,8 @@
 /*
 Exercise 1:
-Write a function testNum that takes a number as an argument and returns a Promise that tests if the value is less than or greater than the value 10. Log the result to the console.
+Write a function testNum that takes a number as an argument and returns a Promise 
+that tests if the value is less than or greater than 
+the value 10. Log the result to the console.
 */
 
 
@@ -9,7 +11,9 @@ const testNum = () => { };
 
 /*
 Exercise 2:
-Write two functions that use Promises that you can chain! The first function, makeAllCaps(), will take in an array of words and capitalize them, and then the second function, sortWords(), will sort the words in alphabetical order. If the array contains anything but strings, it should throw an error.
+Write two functions that use Promises that you can chain! 
+The first function, makeAllCaps(), will take in an array of words and capitalize them, and then the second function, sortWords(),
+ will sort the words in alphabetical order. If the array contains anything but strings, it should throw an error.
 Then call these functions by *chaining* the promises
 */
 
